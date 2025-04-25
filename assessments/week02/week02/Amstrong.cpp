4e680494c8e47a1fed274a5c3c646e5b7e7f0d1c
ABHIJITH JOSE <150343745+Abhijith1399@users.noreply.github.com>
2025-04-25 18:17:39 +0530

    Update Amstrong.cpp

--- assessments/week02/week02/Amstrong.cpp
+++ assessments/week02/week02/Amstrong.cpp
@@ -1,2 +1,22 @@
 #include<iostram>
 using namespace std;
+int main()
+{
+  int num;
+  int rem;
+  int  val;
+  cout<<"enter the number";
+  cin>>num;
+
+  rem=num%10;
+  num=num/10;
+  rem=num;
+int   count++
+if((count*count*count)==num);
+{
+ cout<<"true";
+}
+  else{
+    cout<<false;
+  }
+}
