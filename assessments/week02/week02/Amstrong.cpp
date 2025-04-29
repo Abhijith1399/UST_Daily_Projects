726f00e38ce3c0a3dc13c1641d62310e024d0ad8
ABHIJITH JOSE <150343745+Abhijith1399@users.noreply.github.com>
2025-04-29 13:53:04 +0530

    Amstrong.cpp

--- assessments/week02/week02/Amstrong.cpp
+++ assessments/week02/week02/Amstrong.cpp
@@ -1,22 +1,43 @@
-#include<iostram>
+
+#include<iostream>
 using namespace std;
+
+int getNumberOfDigits(int);
+int powerOfDigits(int, int);
+
 int main()
 {
-  int num;
-  int rem;
-  int  val;
-  cout<<"enter the number";
-  cin>>num;
-
-  rem=num%10;
-  num=num/10;
-  rem=num;
-int   count++
-if((count*count*count)==num);
-{
- cout<<"true";
+	int num;
+	cout << "Enter the input: ";
+	cin >> num;
+	cout << endl;
+	if (num == powerOfDigits(num, getNumberOfDigits(num)))
+		cout << "Armstrong Number";
+	else
+		cout << "Not an Armstrong Number";
+	cout << endl;
+	return 0;
 }
-  else{
-    cout<<false;
-  }
+
+
+int getNumberOfDigits(int num)
+{
+	int count = 0;
+	for (;num > 0;num /= 10)
+		count++;
+	return count;
+}
+
+int powerOfDigits(int num, int pow)
+{
+	int sum = 0, rem, product;
+	for (;num > 0;num /= 10)
+	{
+		product = 1;
+		rem = num % 10;
+		for (int itr = 1; itr <= pow; ++itr)
+			product *= rem;
+		sum += product;
+	}
+	return sum;
 }
