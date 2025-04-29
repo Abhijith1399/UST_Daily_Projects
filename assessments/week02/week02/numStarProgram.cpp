b1f57d545a28b1d6a0b3a0678ddf70aab9fecaa1
ABHIJITH JOSE <150343745+Abhijith1399@users.noreply.github.com>
2025-04-29 11:31:09 +0530

    numStarProgram.cpp

--- assessments/week02/week02/numStarProgram.cpp
+++ assessments/week02/week02/numStarProgram.cpp
@@ -1,38 +1,51 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-    int n, i, j, k;
-    cout << "enter the num" << endl;
+
+int main() {
+    int n;
+    cout << "Enter the number: ";
     cin >> n;
 
-
-    for (i =1;i <= n;i++)
-        cout << "*";
+    cout << "*" << endl;
+    for (int i = 1;i <= n;i++)
     {
-      
-        for (j =1;j <= i-n;j++)
+        cout << "*";
+        for (int j = 1;j <= i;j++)
+
         {
             cout << j;
-
-            for(k=1;)
-            cout << "*";
         }
+
+        for (int k = i - 1;k >= 1;k--)
+        {
+            cout << k;
+
+        }
+        cout << "*";
+        cout << endl;
+    }
+    //second half
+    for (int i = 0;i <= n;i++)
+    {
+        cout << "*";
+        for (int l = 1;l <= n - i;l++)
+        {
+            cout << l;
+        }
+        for (int m = n - 1;m <= 1;m--)
+        {
+            cout << m;
+        }
+    
+    }
+        
+
+        cout << endl;
     }
 
 
-    for (i =n-1;i >=n;i--)
-        cout << "*";
-    {
 
-        for (j = 1;j >=i - n;j++)
-        {
-            cout << j;
 
-        }
-    }
     return 0;
-
-
 
 }
