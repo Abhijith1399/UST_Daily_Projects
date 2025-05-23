237980067acef0555d2cb04fcc34598f650a4874
Abhijith1399 <150343745+Abhijith1399@users.noreply.github.com>
2025-05-23 12:52:08 +0530

    project

---
+++ assessments/food_delivery/food_delivery.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Deliverytip
+{
+private:
+	int orderid;
+	int billamnt;
+	float kilometer;
+	int tip;
+public:
+
+	Deliverytip(int oid,int amt,int km)
+	{
+
+	}
+
+	void setorderid(int);
+	int getorderid();
+	void setbillamnt(int);
+	int getbillamnt();
+	void setkilometer(float);
+	float getkilometer();
+	void settip(int);
+	int getip();
+	int calculatecount();
+	void printDetails();
+
+
+};
+void  Deliverytip::setorderid(int)
+{
+
+}
+int  Deliverytip::getorderid()
+{
+
+}
+void Deliverytip::setbillamnt(int)
+{
+
+}
+int  Deliverytip::getbillamnt()
+{
+
+}
+void Deliverytip::setkilometer(float)
+{
+
+}
+float Deliverytip::getkilometer()
+{
+
+}
+void Deliverytip::settip(int)
+{
+
+}
+int Deliverytip::getip()
+{
+
+}
+int Deliverytip::calculatecount()
+{
+
+}
+void Deliverytip::printDetails()
+{
+
+}
+
+
+
+int main()
+{
+	Deliverytip obj(0,0,0);
+
+
+
+}
